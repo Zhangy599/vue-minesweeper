1b558d2c71bcf28cf9cb565220ba10c1aa99808b
zy599 <767927053@qq.com>
2022-04-08 17:29:20 +0800

    first commit

---
+++ src/pages/index.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+interface BlockState {
+  x: number
+  y: number
+  revealed?: boolean
+  mine?: boolean
+  flagged?: boolean
+  adjacentMines?: number
+}
+const WIDTH = 10
+const HEIGHT = 10
+const state = reactive(Array.from({ length: HEIGHT }, (_, y) => {
+  return Array.from({ length: WIDTH }, (_, x): BlockState => ({
+    x, y,
+  }))
+}))
+function onClick(x: number, y: number) {
+  console.log(x, y)
+}
+</script>
+
+<template>
+  <div>
+    Minesweeper
+    <div v-for="row, y in state" :key="y">
+      <button
+        v-for="item, x in row"
+        :key="x"
+        w-8
+        y-8
+        border="~"
+        hover:bg-gray
+        @click="onClick(x, y)"
+      >
+        {{ item }}
+      </button>
+    </div>
+  </div>
+</template>
